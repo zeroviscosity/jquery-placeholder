13ef2959d8a0a3d0ed8b05bcfa42308bb95daf9b
Kent English <kent.english@gmail.com>
2013-05-04 18:42:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+### jQuery Placeholder
+
+Add the following class to your CSS:
+
+    .placeholder { color: #bbbbbb; }
+
+Add the following to your web page:
+
+	<script src="jquery.placeholder.js"></script>
+
+Add this to your JavaScript:
+
+    $(document).ready(function() {
+      $(input).placeholder();
+    });
---
+++ jquery.placeholder.js
@@ -0,0 +1,39 @@
+/*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+* jQuery Placeholder
+* Kent English 2013
+* http://zeroviscosity.com
+*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+
+;(function($) {
+    $.fn.placeholder = function(options) {
+        return this.each(function() {
+            if (this.hasOwnProperty('placeholder')) {
+                return;
+            }
+
+            var input = $(this),
+                placeholder = input.attr('placeholder'),
+                add = function() {
+                    if (input.val() === '') {
+                        input.val(placeholder);
+                        input.addClass('placeholder');
+                    }
+                },
+                remove = function() {
+                    if (input.val() === placeholder) {
+                        input.val('');
+                        input.removeClass('placeholder');
+                    }
+                };
+            if (typeof placeholder !== 'undefined' && placeholder !== null) {
+                add();
+    
+                input.focus(function() {
+                    remove();
+                }).blur(function() {
+                    add();
+                });
+            }
+        });
+    }
+})(jQuery);
